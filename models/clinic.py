f08d7976afcbf9523c73ab0a018d660b9f94663c
JavGog <javierggjavier@gmail.com>
2022-07-14 15:46:23 -0700

    Clinic class set up

---
+++ models/clinic.py
@@ -0,0 +1,10 @@
+class Clinic:
+
+    def __init__(self, id):
+        self.id = id
+    
+    
+    def adding_vet_to_animal(self, vet, animal):
+        self.vet = vet
+        self.animal = animal
+
