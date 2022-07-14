c404a900a1b35c0dc9edcd33f934bd652f823d41
JavGog <javierggjavier@gmail.com>
2022-07-14 15:36:05 -0700

    Animal class set up

---
+++ models/animal.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self, id, name, date_of_birth, type, phone, treatment):
+        self.id = id
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.type = type
+        self.phone = phone
+        self.treatment = treatment
+
+    
