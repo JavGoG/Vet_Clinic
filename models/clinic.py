5a05afb725021fc260606c55163030cba3955d48
JavGog <javierggjavier@gmail.com>
2022-07-14 19:16:07 -0700

    adding time parameter to Clinic method adding_vet_to_animal

--- models/clinic.py
+++ models/clinic.py
@@ -4,7 +4,8 @@ class Clinic:
         self.id = id
     
     
-    def adding_vet_to_animal(self, vet, animal):
+    def adding_vet_to_animal(self, vet, animal, time):
         self.vet = vet
         self.animal = animal
+        self.time = time
 
