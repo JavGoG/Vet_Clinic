354a14e860a714689552dd76281539bf54b12d75
JavGog <javierggjavier@gmail.com>
2022-07-16 14:24:05 -0700

    animal.py adding owner property to constructor

--- models/animal.py
+++ models/animal.py
@@ -1,10 +1,11 @@
 class Animal:
-    def __init__(self, id, name, date_of_birth, type, phone, treatment):
+    def __init__(self, id, name, date_of_birth, type, owner, treatment):
         self.id = id
         self.name = name
         self.date_of_birth = date_of_birth
         self.type = type
-        self.phone = phone
+        self.owner = owner
         self.treatment = treatment
 
     
+
