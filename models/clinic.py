5495319301b70728a1486fcbde3c7a2779587735
JavGog <javierggjavier@gmail.com>
2022-07-16 10:21:32 -0700

    updated Clinic constructor

--- models/clinic.py
+++ models/clinic.py
@@ -1,8 +1,11 @@
 class Clinic:
 
-    def __init__(self, id):
+    def __init__(self, description, user, duration, completed = False,  id = None, ):
+        self.description = description
+        self.user = user
+        self.duration = duration
+        self.completed = completed
         self.id = id
-    
     
     def adding_vet_to_animal(self, vet, animal, time):
         self.vet = vet
